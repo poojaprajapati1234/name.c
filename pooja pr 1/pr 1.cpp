cb2b945a0b4eba380ccc142214c2b701c2faa208
poojaprajapati1234 <poojaprajapati22906@gmail.com>
2024-10-04 18:53:58 +0530

    Add files via upload

---
+++ pooja pr 1/pr 1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	float celsius,fahrenheit;
+	
+	
+	printf("enter the value of celsius ");
+	scanf("%f",&celsius);
+	
+	fahrenheit = (1.8 *celsius );
+	printf("the fahrenheit is %f ",fahrenheit);
+		
+	
+}
